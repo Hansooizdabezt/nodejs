87f38f2824c169abd51513a15e0bbe7674657111
hansoo280102 <tantran012080@gmail.com>
2025-02-12 20:57:29 +0700

    19

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 8080
+require('dotenv').config()
+
+const port = process.env.PORT || 3001
+const hostname = process.env.HOST_NAME
 
 //cấu hình view engine EJS
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
     res.render('sample')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`App listening on http://localhost:${port}`)
 })
