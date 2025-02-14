12a2888daa6d398d6bd7a22dfc6dd58e35ccc103
hansoo280102 <tantran012080@gmail.com>
2025-02-14 22:49:11 +0700

    32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,17 @@
-const getHomePage = (req, res) => {
-    res.render('sample')
+const connection = require('../config/database')
+
+const getHomePage = async (req, res) => {
+    let users = [];
+    // A simple SELECT query
+    connection.query(
+        'SELECT * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log("results: ", results);
+
+            res.send(JSON.stringify(users));
+        }
+    );
+
 }
 module.exports = { getHomePage }
--- src/server.js
+++ src/server.js
@@ -2,18 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-const connection = require('./config/database')
-
-
-// A simple SELECT query
-connection.query(
-    'SELECT * FROM Users u',
-    function (err, results, fields) {
-        console.log("results: ", results);
-        console.log("Error connect database: ", err);
-    }
-);
-
 
 const app = express()
 const port = process.env.PORT || 3001
