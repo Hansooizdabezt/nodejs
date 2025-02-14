5666808978998c5552675bf3696ca9b992319f86
hansoo280102 <tantran012080@gmail.com>
2025-02-14 22:24:39 +0700

    30

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 require('dotenv').config()
 const express = require('express')
-const configViewEngine = require('./config/viewEngine');
+const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2')
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// A simple SELECT query
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log("results: ", results);
+        console.log("Error connect database: ", err);
+    }
+);
+
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -11,6 +31,9 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app);
 
 app.use("/", webRoutes);
+
+// test connection
+
 
 app.listen(port, hostname, () => {
     console.log(`App listening on http://localhost:${port}`)
