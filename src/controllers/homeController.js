54e4c0a650964f796941654fba58e07f1a37e551
hansoo280102 <tantran012080@gmail.com>
2025-02-15 19:57:04 +0700

    35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,7 @@
 const connection = require('../config/database')
 
 const getHomePage = async (req, res) => {
-    let users = [];
-    // A simple SELECT query
-    connection.query(
-        'SELECT * FROM Users u',
-        function (err, results, fields) {
-            users = results;
-            console.log("results: ", results);
-
-            res.send(JSON.stringify(users));
-        }
-    );
+    return res.render('../views/home.ejs')
 
 }
 module.exports = { getHomePage }
